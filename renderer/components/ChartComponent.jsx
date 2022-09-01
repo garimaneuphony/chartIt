8f5e72899eb1702d17c696968dcd47919d1b3b0f
krgarima <kumargarima59@gmail.com>
2022-09-01 14:17:41 +0530

    feat: graph of 6 channels

--- renderer/components/ChartComponent.jsx
+++ renderer/components/ChartComponent.jsx
@@ -9,20 +9,21 @@ import {
   ReferenceLine
 } from "recharts";
 
-export default function Chart({Data}) {
+export default function Chart({Data, label}) {
 
   return (
- <ResponsiveContainer width="100%" aspect={8}>
+ <ResponsiveContainer width="100%" aspect={10}>
   <LineChart
   data={Data.toArray()}
 >
 <XAxis dataKey="timePoint" /> 
   <ReferenceLine y={0} stroke="lightgray" />
-  <YAxis type="number" domain={[-75, 75]} allowDataOverflow={true} tick={false} label={{ value: 'Fp1', fill: 'white' }} />
+  <YAxis type="number" domain={[-75, 75]} allowDataOverflow={true} tick={false} label={{ value: label, fill: 'white' }} />
   <Line
     dataKey="dataPoint"
     stroke="white"
     dot={false}
+    key={label}
   />
 </LineChart>
 </ResponsiveContainer> ) 
--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -9,30 +9,64 @@ function Next() {
   const [isActive, setActive] = useState(true);
   const [buttonText, setButtonText] = useState('Fetch Data Stream');
   const [listenerCount, setCount] = useState(0);
-  const DataQueue = useRef(new Queue)
+  const DataQueueOfFp1 = useRef(new Queue);
+  const DataQueueOfFp2 = useRef(new Queue);
+  const DataQueueOfF3 = useRef(new Queue);
+  const DataQueueOfF4 = useRef(new Queue);
+  const DataQueueOfFz = useRef(new Queue);
+  const DataQueueOfPz = useRef(new Queue);
   const [packets, setDataPackets]  = useState([]);
   const mycount = useRef(0)
 
   const Remover = () => {
-    DataQueue.current.dequeue();
+    DataQueueOfFp1.current.dequeue();
+    DataQueueOfFp2.current.dequeue();
+    DataQueueOfF3.current.dequeue();
+    DataQueueOfF4.current.dequeue();
+    DataQueueOfFz.current.dequeue();
+    DataQueueOfPz.current.dequeue();
   }
 
-  const helper = packets => {
+  const helper = (packets) => {
     if (packets) {
-      DataQueue.current.enqueue({
+      DataQueueOfFp1.current.enqueue({
         name: "channel - 1",
         dataPoint: packets[0],
         // timePoint: packets[8]
         timePoint: mycount.current
       })
-      console.log(DataQueue.current.toArray())
+      DataQueueOfFp2.current.enqueue({
+        name: "channel - 2",
+        dataPoint: packets[1],
+        timePoint: mycount.current
+      })
+      DataQueueOfF3.current.enqueue({
+        name: "channel - 2",
+        dataPoint: packets[2],
+        timePoint: mycount.current
+      })
+      DataQueueOfF4.current.enqueue({
+        name: "channel - 2",
+        dataPoint: packets[3],
+        timePoint: mycount.current
+      })
+      DataQueueOfFz.current.enqueue({
+        name: "channel - 2",
+        dataPoint: packets[6],
+        timePoint: mycount.current
+      })
+      DataQueueOfPz.current.enqueue({
+        name: "channel - 2",
+        dataPoint: packets[7],
+        timePoint: mycount.current
+      })
+      console.log(DataQueueOfFp1.current.toArray())
       mycount.current++
-      if (mycount.current > 1000) {
+      if (mycount.current > 999) {
         Remover()
       }
     }
   }
-
 
   const getData = () => {
     ipcRenderer.on("device-data",(event,packets)=>{
@@ -76,8 +110,12 @@ function Next() {
 
       <div id="chartIt"> 
       <span className='mt-4 w-full flex-wrap flex justify-center'>⚡  Render Chart Here ⚡</span> 
-      <Chart Data={DataQueue.current} />
-     
+      <Chart Data={DataQueueOfFp1.current} label={'Fp1'} />
+      <Chart Data={DataQueueOfFp2.current} label={'Fp2'} />
+      <Chart Data={DataQueueOfF3.current} label={'F3'} />
+      <Chart Data={DataQueueOfF4.current} label={'F4'} />
+      <Chart Data={DataQueueOfFz.current} label={'Fz'} />
+      <Chart Data={DataQueueOfPz.current} label={'Pz'} />
       </div> 
 
       <div className='mt-10 w-full flex-wrap flex justify-center'>
